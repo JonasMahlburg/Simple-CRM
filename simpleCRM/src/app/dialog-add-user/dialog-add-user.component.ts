ace962908082df0193571505061f3423cc13c907
Jonas Mahlburg <darrylmanga@yahoo.com>
2024-09-23 10:47:17 +0200

    added 'add User mask'

---
+++ simpleCRM/src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+
+
+
+
+@Component({
+  selector: 'app-dialog-add-user',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatDatepickerModule,],
+  templateUrl: './dialog-add-user.component.html',
+  styleUrl: './dialog-add-user.component.scss',
+  providers: [provideNativeDateAdapter()],
+})
+export class DialogAddUserComponent {
+
+}
